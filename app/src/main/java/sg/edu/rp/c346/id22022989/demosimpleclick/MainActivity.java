586bd0863da7992fed07269676c8cf44219cf6ff
ASUS <22022989@myrp.edu.sg>
2023-05-03 16:03:35 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22022989/demosimpleclick/MainActivity.java
@@ -0,0 +1,74 @@
+package sg.edu.rp.c346.id22022989.demosimpleclick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvDisplay;
+    Button btnDisplay;
+    EditText etInput;
+    ToggleButton tbtn;
+    RadioGroup rgGender;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvDisplay = findViewById(R.id.textViewDisplay);
+        btnDisplay = findViewById(R.id.buttonDisplay);
+        etInput = findViewById(R.id.editTextInput);
+        tbtn = findViewById(R.id.toggleButtonEnabled);
+        rgGender = findViewById(R.id.radioGroupGender);
+
+        btnDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String stringResponse = etInput.getText().toString();
+                tvDisplay.setText(stringResponse);
+            }
+        });
+
+        tbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (tbtn.isChecked()) {
+                    etInput.setEnabled(true);
+                } else {
+                    etInput.setEnabled(false);
+                }
+            }
+        });
+
+        btnDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Code for the action
+                String stringResponse = etInput.getText().toString();
+                int checkedRadioId = rgGender.getCheckedRadioButtonId();
+                if(checkedRadioId == R.id.radioButtonMale){
+                    String sR = "He says " + stringResponse;
+                    tvDisplay.setText(sR);
+                }else{
+                    // Write the code when female selected
+                    String sR = "She says " + stringResponse;
+                    tvDisplay.setText(sR);
+                }
+
+            }
+        });
+
+
+
+    }}
